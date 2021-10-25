63343df79e76a2af05df10befbfbb073d2651646
brianorora <brianorora@gmail.com>
2021-10-26 01:10:58 +0300

    Add dynamic routing to `About` component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link, Route, useRouteMatch } from 'react-router-dom';
 
-const About = () => {
-  return <div>Hello from about page</div>
+import SinglePage from './SinglePage';
+
+const About = (props) => {
+  console.log('route:', useRouteMatch());
+  const { url, path} = useRouteMatch();
+  return (<div>
+    <ul>
+      <li>
+        <Link to={`${url}/about-app`}>About App</Link>
+      </li>
+      <li>
+        <Link to={`${url}/about-author`}>About Author</Link>
+      </li>
+    </ul>
+    <Route path={`${path}/:slug`}>
+      <SinglePage />
+    </Route>
+  </div>
+  )
 }
 
 export default About;
