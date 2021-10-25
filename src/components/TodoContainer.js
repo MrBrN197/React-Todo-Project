bfbbd2d6f227f42277304fb07f7f6079ccceb851
brianorora <brianorora@gmail.com>
2021-10-25 19:53:36 +0300

    Add `uuid`

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -4,22 +4,24 @@ import TodosList from './TodoList';
 import Header from './Header';
 import InputTodo from './InputTodo';
 
+import { v4 as uuidv4 } from 'uuid';
+
 import React from "react"
 const TodoContainer = () => {
   const [state, setState] = useState({
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Setup development environment",
         completed: true
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Develop website and add content",
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Deploy to live server",
         completed: false
       }
@@ -42,7 +44,7 @@ const TodoContainer = () => {
   const addTodoItem = title => {
     console.log(title);
     const newTodo = {
-      id: 4,
+      id: uuidv4(),
       title,
       completed: false
     }
