54bb1f133891875054fb191b81f24b527f69487e
brianorora <brianorora@gmail.com>
2021-10-26 16:33:14 +0300

    Add github pages basename to `<Router />`

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import TodoContainer from './components/TodoContainer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
